95d5ef5330fff3d85fd94bdbf6ff06deee00c7a8
Edgar256 <tinka.edgar@gmail.com>
2018-07-13 16:13:10 +0300

    changes to index file

--- index.php
+++ index.php
@@ -26,13 +26,13 @@
      //Perform calculation
      var BMI = weight / Math.pow(height, 2);
      //Display result of calculation
-document.getElementById("output").innerHTML = Math.round(BMI * 100)/100;
-     if (BMI < 18.5) document.getElementById("comment").innerHTML = "Underweight";
-     if (BMI >= 18.5 && BMI <= 25) document.getElementById("comment").innerHTML = "Normal";
-     if (BMI >= 25 && BMI <= 30) document.getElementById("comment").innerHTML = "Obese";
-     if (BMI > 30) document.getElementById("comment").innerHTML = "Overweight";
-     document.getElementById("answer").value = output;
- }
+      document.getElementById("output").innerHTML = Math.round(BMI * 100)/100;
+           if (BMI < 18.5) document.getElementById("comment").innerHTML = "Underweight";
+           if (BMI >= 18.5 && BMI <= 25) document.getElementById("comment").innerHTML = "Normal";
+           if (BMI >= 25 && BMI <= 30) document.getElementById("comment").innerHTML = "Obese";
+           if (BMI > 30) document.getElementById("comment").innerHTML = "Overweight";
+           document.getElementById("answer").value = output;
+       }
   </script>
 </head>
 <body>
